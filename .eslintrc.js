5b8627b70bd8c722435737414be28b37d5707a6b
yanwei0433 <yanwei0433@163.com>
2025-06-25 10:01:03 +0800

    更新

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
   ],
   env: {
     node: true,
@@ -18,12 +17,9 @@ module.exports = {
     // 基本规则
     'no-console': 'warn',
     'no-debugger': 'warn',
-    'no-unused-vars': 'off', // 使用TypeScript的规则
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-unused-vars': 'warn',
     
     // TypeScript特定规则
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-non-null-assertion': 'warn',
     
